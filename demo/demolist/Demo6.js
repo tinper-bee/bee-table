d4c15e2d2f78bab9267a00c065913d4f05c0b821
huyueb <1062887235@qq.com>
2017-11-24 10:59:12 +0800

    修改table的td的wordbreak样式

--- demo/demolist/Demo6.js
+++ demo/demolist/Demo6.js
@@ -18,14 +18,7 @@ const columns6 = [
     key: "name"
   },
   { title: "Age", width: 100, dataIndex: "age", key: "age"},
-  { title: "Column 1", dataIndex: "address", key: "1" },
-  { title: "Column 2", dataIndex: "address", key: "2" },
-  { title: "Column 3", dataIndex: "address", key: "3" },
-  { title: "Column 4", dataIndex: "address", key: "4" },
-  { title: "Column 5", dataIndex: "address", key: "5" },
-  { title: "Column 6", dataIndex: "address", key: "6" },
-  { title: "Column 7", dataIndex: "address", key: "7" },
-  { title: "Column 8", dataIndex: "address", key: "8" }
+  { title: "Address", dataIndex: "address", key: "1" }
 ];
 
 const data6 = [
