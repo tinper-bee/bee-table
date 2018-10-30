e2abf8b8dcc24c901230f4f7cd36b1739bf460d5
wanghaoo <wanghaoo@yonyou.com>
2018-10-30 16:40:25 +0800

    feat(表头支持滚动条):

--- build/Table.js
+++ build/Table.js
@@ -707,7 +707,8 @@ var Table = function (_Component) {
         _props3$scroll = _props3.scroll,
         scroll = _props3$scroll === undefined ? {} : _props3$scroll,
         getBodyWrapper = _props3.getBodyWrapper,
-        footerScroll = _props3.footerScroll;
+        footerScroll = _props3.footerScroll,
+        headerScroll = _props3.headerScroll;
     var useFixedHeader = this.props.useFixedHeader;
 
     var bodyStyle = _extends({}, this.props.bodyStyle);
@@ -739,8 +740,18 @@ var Table = function (_Component) {
       // Add negative margin bottom for scroll bar overflow bug
       var scrollbarWidth = (0, _utils.measureScrollbar)();
       if (scrollbarWidth >= 0) {
-        (fixed ? bodyStyle : headStyle).marginBottom = '-' + scrollbarWidth + 'px';
         (fixed ? bodyStyle : headStyle).paddingBottom = '0px';
+        //显示表头滚动条
+        if (headerScroll) {
+          if (fixed) {
+            bodyStyle.marginBottom = '-' + scrollbarWidth + 'px';
+            headStyle.marginBottom = scrollbarWidth + 'px';
+          } else {
+            headStyle.marginBottom = '0px';
+          }
+        } else {
+          (fixed ? bodyStyle : headStyle).marginBottom = '-' + scrollbarWidth + 'px';
+        }
       }
     }
 
