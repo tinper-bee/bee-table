097537ef7596bbed463c3f913d3a3062f5014be6
wanghaoo <wanghaoo@yonyou.com>
2018-12-24 10:56:36 +0800

    合计示例

--- demo/demolist/Demo13.js
+++ demo/demolist/Demo13.js
@@ -13,283 +13,65 @@ import Icon from "bee-icon";
 import multiSelect from "../../src/lib/multiSelect.js";
 import sort from "../../src/lib/sort.js";
 import sum from "../../src/lib/sum.js";
-const ComplexTable = sum(Table);
+
 const columns13 = [
-
-
-  // {
-  //     title: "项目期数主键",
-  //     dataIndex: "cprojectId",
-  //     key: "cprojectId",
-  //     width: 150,
-  //     render: (text, record, index) => this.
-  //         renderReflc_qs
-  //         (text, record, index, "cprojectId", this.editFlag)
-  // },
-
-  // {
-  //     title: "项目期数编码",
-  //     dataIndex: "cprojectPeriodCode",
-  //     key: "cprojectPeriodCode",
-  //     width: 150,
-  //     render: (text, record, index) => this.
-  //         renderColumns
-
-  //         (text, record, index, "cprojectPeriodCode", this.editFlag)
-  // },
   {
-      title: "序号",
-      dataIndex: "index",
-      key: "index",
-      width: 100,
-      render(record, text, index) {
-          return index + 1;
-      }
-  },
-
-  {
-      title: "项目期数",
-      dataIndex: "cprojectPeriod",
-      key: "cprojectPeriod",
-      width: 150,
-      // render: (text, record, index) => this.
-      //     renderColumns
-      //     (text, record, index, "cprojectPeriod", this.editFlag)
-  },
-  // {
-  //     title: "期数",
-  //     dataIndex: "cprojectShow",
-  //     key: "cprojectShow",
-  //     width: 150,
-  //     render: (text, record, index) => this.
-  //         renderColumns
-  //         (text, record, index, "cprojectShow", this.editFlag)
-  // },
-  {
-      title: "业态",
-      dataIndex: "ctype",
-      key: "ctype",
-      width: 150,
-      
-      // render: (text, record, index) => this.
-
-      //     renderReflc_yt
-      //     (text, record, index, "cType", this.editFlag)
-  },
-  // {
-  //     title: "业态",
-  //     dataIndex: "ctypeShow",
-  //     key: "ctypeShow",
-  //     width: 150,
-  //     render: (text, record, index) => this.
-  //         renderColumns
-  //         (text, record, index, "ctypeShow", this.editFlag)
-  // },
-
-  // {
-  //     title: "房间id",
-  //     dataIndex: "croomId",
-  //     key: "croomId",
-  //     width: 150,
-  //     render: (text, record, index) => this.
-  //         renderColumns
-  //         (text, record, index, "croomId", this.editFlag)
-  // },
-  {
-      title: "房间号码",
-      dataIndex: "croomNumber",
-      key: "croomNumber",
-      width: 200,
-      
-      
+    title: "名字",
+    dataIndex: "a",
+    key: "a",
+    className:'dfasd',
+    width: 200
   },
   {
-      title: "客户",
-      dataIndex: "ccustomer",
-      key: "ccustomer",
-      width: 150,
-      
+    title: "功力指数",
+    dataIndex: "b",
+    key: "b",
+    width: 200,
+    sumCol: true,
+    sorter: (a, b) => a.c - b.c,
+    sorterClick:(data,type)=>{//排序的回调函数
+      //type value is up or down
+      console.log("data",data);
+    }
   },
   {
-      title: "合同面积",
-      dataIndex: "carea",
-      key: "carea",
-      width: 250,
-      sumCol: true,
-    
+    title: "年龄",
+    dataIndex: "c",
+    key: "c",
+    width: 200,
+    sumCol: true,
+    sorter: (a, b) => a.c - b.c,
+    sorterClick:(data,type)=>{//排序的回调函数
+      //type value is up or down
+      console.log("data",data);
+    }
   },
   {
-      title: "合同金额",
-      dataIndex: "camount",
-      key: "camount",
-      width: 250,
-      sumCol: true,
-     
+    title: "成绩",
+    dataIndex: "e",
+    key: "e",
+    width: 200,
+    sumCol: true,
+    sorter: (a, b) => a.c - b.c,
   },
   {
-      title: "是否网签/备案",
-      dataIndex: "crecord",
-      key: "crecord",
-      width: 150,
-      // render: (text, record, index) => this.
-      //     renderSelect
-      //     (text, record, index, "crecord", this.editFlag)
-  },
-  {
-      title: "付款方式",
-      dataIndex: "cpayType",
-      key: "cpayType",
-      width: 150,
-     
-  },
-  {
-      title: "是否清款",
-      dataIndex: "coverbz",
-      key: "coverbz",
-      width: 150,
-      // render: (text, record, index) => this.
-      //     renderSelect
-      //     (text, record, index, "coverbz", this.editFlag)
-  },
-  {
-      title: "签订日期",
-      dataIndex: "corderDate",
-      key: "corderDate",
-      width: 140,
-     
-  },
-  {
-      title: "应收日期",
-      dataIndex: "cpayDate",
-      key: "cpayDate",
-      width: 140,
-      
-  },
-  {
-      title: "营改增前预收款",
-      dataIndex: "cbeforeFee",
-      key: "cbeforeFee",
-      width: 250,
-      sumCol: true,
-
-  },
-  {
-      title: "财务收款额",
-      dataIndex: "cncAccount",
-      key: "cncAccount",
-      width: 250,
-      sumCol: true,
-      
-  },
-  {
-      title: "营改增后预收款",
-      dataIndex: "cafterFee",
-      key: "cafterFee",
-      width: 250,
-      sumCol: true,
-      
-  },
-  {
-      title: "预收房款余额",
-      dataIndex: "chouseFee",
-      key: "chouseFee",
-      width: 250,
-      sumCol: true,
-    
-  },
-  {
-      title: "完工进度(%)",
-      dataIndex: "cprogress",
-      key: "cprogress",
-      width: 150,
-      // render: (text, record, index) => this.
-      //     renderColumnsFloat
-      //     (text, record, index, "cProgress", this.editFlag)
-  },
-  {
-      title: "上期累计营业税",
-      dataIndex: "cpreviousBusinessTax",
-      key: "cpreviousBusinessTax",
-      width: 250,
-      sumCol: true,
-     
-  },
-  {
-      title: "上期累计增值税",
-      dataIndex: "cpreviousAddTax",
-      key: "cpreviousAddTax",
-      width: 250,
-      sumCol: true,
-      
-  },
-  {
-      title: "本期营业税",
-      dataIndex: "cnowBusinessTax",
-      key: "cnowBusinessTax",
-      width: 250,
-      sumCol: true,
-     
-  },
-  {
-      title: "本期增值税",
-      dataIndex: "cnowAddTax",
-      key: "cnowAddTax",
-      width: 250,
-      sumCol: true,
-    
-  },
-  {
-      title: "上期营改增前累计收入金额",
-      dataIndex: "cpreviousBeforeTotalfee",
-      key: "cpreviousBeforeTotalfee",
-      width: 250,
-      sumCol: true,
-      
-  },
-  {
-      title: "上期营改增后累计收入金额",
-      dataIndex: "cpreviousAfterTotalfee",
-      key: "cpreviousAfterTotalfee",
-      width: 250,
-      sumCol: true,
-
-  },
-  {
-      title: "本期营改增前收入金额",
-      dataIndex: "cnowBeforeTotalfee",
-      key: "cnowBeforeTotalfee",
-      width: 250,
-      sumCol: true,
-     
-  },
-  {
-      title: "本期营改增后收入金额",
-      dataIndex: "cnowAfterTotalfee",
-      key: "cnowAfterTotalfee",
-      width: 250,
-      sumCol: true,
-     
-  },
-  // {
-  //     title: "合计",
-  //     dataIndex: "d",
-  //     key: "d",
-  //     fixed: "right",
-  //     width: 150
-  // }
-  {
-      title: "操作",
-      dataIndex: "d",
-      key: "d",
-      width: 100,
-      fixed: "right",
-      
+    title: "武功级别",
+    dataIndex: "d",
+    key: "d",
+    width: 200
   }
 ];
 
- const data13 =[{"id":"06f2acea44df4b009bced2a31deb89ec","createTime":"2018-12-21 15:14:39","createUser":"8458fab111d34dbb8295d7a66660194c","lastModified":"2018-12-21 15:14:39","lastModifyUser":"8458fab111d34dbb8295d7a66660194c","ts":"2018-12-21 15:14:39","newTs":"2018-12-21 15:20:04 225","dr":0,"bpmState":null,"taskKey":null,"taskId":null,"processInstanceId":null,"processDefineCode":null,"comment":null,"pkFatherRowId":"01667fcea1c34a58ac094f10609dbf46","pzbz":null,"pzscman":null,"pzscdate":null,"ncConsumer":null,"ncIdCard":null,"ncRoomNo":null,"ncPeriodNo":null,"ncTypeNo":null,"tenantid":"tenant","cdef1":null,"cdef2":null,"cdef3":null,"cdef4":null,"cdef5":null,"cdef6":null,"cdef7":null,"cdef8":null,"cdef9":null,"cdef10":null,"cdef11":null,"cdef12":null,"cdef13":null,"cdef14":null,"cdef15":null,"cdef16":null,"cdef17":null,"cdef18":null,"cdef19":null,"cdef20":null,"cprojectPeriod":"一期","ctype":"高层公寓","carea":241,"bpmBillCode":"201812211520042","cprogress":100,"ccustomer":"姚雅珍,高照","croomNumber":"杭州柳岸晓风->12幢->105","cnowBeforeTotalfee":0,"cnowAfterTotalfee":17735733.64,"chouseFee":19509307,"crecord":"是","cprojectPeriodCode":null,"cpayType":"一次性付款","corderDate":"2018-06-09","cpayDate":"2018-06-09","coverbz":"是","cprojectId":null,"camount":19509307,"croomId":null,"ctypeShow":null,"cbeforeFee":0,"cncAccount":19509307,"cafterFee":19509307,"cnowBusinessTax":0,"cnowAddTax":1773573.36,"cpreviousBusinessTax":0,"cpreviousAddTax":0,"cpreviousBeforeTotalfee":0,"cpreviousAfterTotalfee":0,"mainBoCode":"lc_xsr_income_dtl","coverbzEnumValue":null,"crecordEnumValue":null,"cprojectShow":null},{"id":"06f2acea44df4b009bced2a31deb89ec","createTime":"2018-12-21 15:14:39","createUser":"8458fab111d34dbb8295d7a66660194c","lastModified":"2018-12-21 15:14:39","lastModifyUser":"8458fab111d34dbb8295d7a66660194c","ts":"2018-12-21 15:14:39","newTs":"2018-12-21 15:20:04 225","dr":0,"bpmState":null,"taskKey":null,"taskId":null,"processInstanceId":null,"processDefineCode":null,"comment":null,"pkFatherRowId":"01667fcea1c34a58ac094f10609dbf46","pzbz":null,"pzscman":null,"pzscdate":null,"ncConsumer":null,"ncIdCard":null,"ncRoomNo":null,"ncPeriodNo":null,"ncTypeNo":null,"tenantid":"tenant","cdef1":null,"cdef2":null,"cdef3":null,"cdef4":null,"cdef5":null,"cdef6":null,"cdef7":null,"cdef8":null,"cdef9":null,"cdef10":null,"cdef11":null,"cdef12":null,"cdef13":null,"cdef14":null,"cdef15":null,"cdef16":null,"cdef17":null,"cdef18":null,"cdef19":null,"cdef20":null,"cprojectPeriod":"一期","ctype":"高层公寓","carea":241,"bpmBillCode":"201812211520042","cprogress":100,"ccustomer":"姚雅珍,高照","croomNumber":"杭州柳岸晓风->12幢->105","cnowBeforeTotalfee":0,"cnowAfterTotalfee":17735733.64,"chouseFee":19509307,"crecord":"是","cprojectPeriodCode":null,"cpayType":"一次性付款","corderDate":"2018-06-09","cpayDate":"2018-06-09","coverbz":"是","cprojectId":null,"camount":19509307,"croomId":null,"ctypeShow":null,"cbeforeFee":0,"cncAccount":19509307,"cafterFee":19509307,"cnowBusinessTax":0,"cnowAddTax":1773573.36,"cpreviousBusinessTax":0,"cpreviousAddTax":0,"cpreviousBeforeTotalfee":0,"cpreviousAfterTotalfee":0,"mainBoCode":"lc_xsr_income_dtl","coverbzEnumValue":null,"crecordEnumValue":null,"cprojectShow":null},{"id":"06f2acea44df4b009bced2a31deb89ec","createTime":"2018-12-21 15:14:39","createUser":"8458fab111d34dbb8295d7a66660194c","lastModified":"2018-12-21 15:14:39","lastModifyUser":"8458fab111d34dbb8295d7a66660194c","ts":"2018-12-21 15:14:39","newTs":"2018-12-21 15:20:04 225","dr":0,"bpmState":null,"taskKey":null,"taskId":null,"processInstanceId":null,"processDefineCode":null,"comment":null,"pkFatherRowId":"01667fcea1c34a58ac094f10609dbf46","pzbz":null,"pzscman":null,"pzscdate":null,"ncConsumer":null,"ncIdCard":null,"ncRoomNo":null,"ncPeriodNo":null,"ncTypeNo":null,"tenantid":"tenant","cdef1":null,"cdef2":null,"cdef3":null,"cdef4":null,"cdef5":null,"cdef6":null,"cdef7":null,"cdef8":null,"cdef9":null,"cdef10":null,"cdef11":null,"cdef12":null,"cdef13":null,"cdef14":null,"cdef15":null,"cdef16":null,"cdef17":null,"cdef18":null,"cdef19":null,"cdef20":null,"cprojectPeriod":"一期","ctype":"高层公寓","carea":241,"bpmBillCode":"201812211520042","cprogress":100,"ccustomer":"姚雅珍,高照","croomNumber":"杭州柳岸晓风->12幢->105","cnowBeforeTotalfee":0,"cnowAfterTotalfee":17735733.64,"chouseFee":19509307,"crecord":"是","cprojectPeriodCode":null,"cpayType":"一次性付款","corderDate":"2018-06-09","cpayDate":"2018-06-09","coverbz":"是","cprojectId":null,"camount":19509307,"croomId":null,"ctypeShow":null,"cbeforeFee":0,"cncAccount":19509307,"cafterFee":19509307,"cnowBusinessTax":0,"cnowAddTax":1773573.36,"cpreviousBusinessTax":0,"cpreviousAddTax":0,"cpreviousBeforeTotalfee":0,"cpreviousAfterTotalfee":0,"mainBoCode":"lc_xsr_income_dtl","coverbzEnumValue":null,"crecordEnumValue":null,"cprojectShow":null},{"id":"06f2acea44df4b009bced2a31deb89ec","createTime":"2018-12-21 15:14:39","createUser":"8458fab111d34dbb8295d7a66660194c","lastModified":"2018-12-21 15:14:39","lastModifyUser":"8458fab111d34dbb8295d7a66660194c","ts":"2018-12-21 15:14:39","newTs":"2018-12-21 15:20:04 225","dr":0,"bpmState":null,"taskKey":null,"taskId":null,"processInstanceId":null,"processDefineCode":null,"comment":null,"pkFatherRowId":"01667fcea1c34a58ac094f10609dbf46","pzbz":null,"pzscman":null,"pzscdate":null,"ncConsumer":null,"ncIdCard":null,"ncRoomNo":null,"ncPeriodNo":null,"ncTypeNo":null,"tenantid":"tenant","cdef1":null,"cdef2":null,"cdef3":null,"cdef4":null,"cdef5":null,"cdef6":null,"cdef7":null,"cdef8":null,"cdef9":null,"cdef10":null,"cdef11":null,"cdef12":null,"cdef13":null,"cdef14":null,"cdef15":null,"cdef16":null,"cdef17":null,"cdef18":null,"cdef19":null,"cdef20":null,"cprojectPeriod":"一期","ctype":"高层公寓","carea":241,"bpmBillCode":"201812211520042","cprogress":100,"ccustomer":"姚雅珍,高照","croomNumber":"杭州柳岸晓风->12幢->105","cnowBeforeTotalfee":0,"cnowAfterTotalfee":17735733.64,"chouseFee":19509307,"crecord":"是","cprojectPeriodCode":null,"cpayType":"一次性付款","corderDate":"2018-06-09","cpayDate":"2018-06-09","coverbz":"是","cprojectId":null,"camount":19509307,"croomId":null,"ctypeShow":null,"cbeforeFee":0,"cncAccount":19509307,"cafterFee":19509307,"cnowBusinessTax":0,"cnowAddTax":1773573.36,"cpreviousBusinessTax":0,"cpreviousAddTax":0,"cpreviousBeforeTotalfee":0,"cpreviousAfterTotalfee":0,"mainBoCode":"lc_xsr_income_dtl","coverbzEnumValue":null,"crecordEnumValue":null,"cprojectShow":null},{"id":"06f2acea44df4b009bced2a31deb89ec","createTime":"2018-12-21 15:14:39","createUser":"8458fab111d34dbb8295d7a66660194c","lastModified":"2018-12-21 15:14:39","lastModifyUser":"8458fab111d34dbb8295d7a66660194c","ts":"2018-12-21 15:14:39","newTs":"2018-12-21 15:20:04 225","dr":0,"bpmState":null,"taskKey":null,"taskId":null,"processInstanceId":null,"processDefineCode":null,"comment":null,"pkFatherRowId":"01667fcea1c34a58ac094f10609dbf46","pzbz":null,"pzscman":null,"pzscdate":null,"ncConsumer":null,"ncIdCard":null,"ncRoomNo":null,"ncPeriodNo":null,"ncTypeNo":null,"tenantid":"tenant","cdef1":null,"cdef2":null,"cdef3":null,"cdef4":null,"cdef5":null,"cdef6":null,"cdef7":null,"cdef8":null,"cdef9":null,"cdef10":null,"cdef11":null,"cdef12":null,"cdef13":null,"cdef14":null,"cdef15":null,"cdef16":null,"cdef17":null,"cdef18":null,"cdef19":null,"cdef20":null,"cprojectPeriod":"一期","ctype":"高层公寓","carea":241,"bpmBillCode":"201812211520042","cprogress":100,"ccustomer":"姚雅珍,高照","croomNumber":"杭州柳岸晓风->12幢->105","cnowBeforeTotalfee":0,"cnowAfterTotalfee":17735733.64,"chouseFee":19509307,"crecord":"是","cprojectPeriodCode":null,"cpayType":"一次性付款","corderDate":"2018-06-09","cpayDate":"2018-06-09","coverbz":"是","cprojectId":null,"camount":19509307,"croomId":null,"ctypeShow":null,"cbeforeFee":0,"cncAccount":19509307,"cafterFee":19509307,"cnowBusinessTax":0,"cnowAddTax":1773573.36,"cpreviousBusinessTax":0,"cpreviousAddTax":0,"cpreviousBeforeTotalfee":0,"cpreviousAfterTotalfee":0,"mainBoCode":"lc_xsr_income_dtl","coverbzEnumValue":null,"crecordEnumValue":null,"cprojectShow":null}]
+const data13 = [
+  { a: "杨过", b: 675, c: 30, d: "内行",e:100, key: "2" },
+  { a: "令狐冲", b: 43, c: 41, d: "大侠",e:90, key: "1" },
+  { a: "令狐冲1", b: 43, c: 81, d: "大侠", e:120,key: "4" },
+  { a: "令狐冲2", b: 43, c: 81, d: "大侠", e:130,key: "5" },
+  { a: "郭靖", b: 153, c: 25, d: "大侠",e:90, key: "3" }
+];
+
 //拼接成复杂功能的table组件不能在render中定义，需要像此例子声明在组件的外侧，不然操作state会导致功能出现异常
-// let ComplexTable = multiSelect(sum(sort(Table, Icon)), Checkbox);
+let ComplexTable = multiSelect(sum(sort(Table, Icon)), Checkbox);
 
 class Demo13 extends Component {
   constructor(props) {
@@ -342,8 +124,7 @@ class Demo13 extends Component {
           columns={columns13}
           data={this.state.data13}
           multiSelect={multiObj}
-          bordered
-          scroll={{ x: '100%', y: 100 }}
+          sort={sortObj}
           getSelectedDataFunc={this.getSelectedDataFunc}
         />
       </div>
---
+++ demo/demolist/Demo32.js
@@ -0,0 +1,112 @@
+/**
+*
+* @title 大数据加载下的复杂Table
+*
+* @description
+*/
+
+import React, { Component } from "react";
+import Tooltip from "bee-tooltip";
+import Checkbox from 'bee-checkbox';
+import Icon from 'bee-icon';
+import Popover from 'bee-popover';
+import Table from "../../src";
+import BigData from "../../src/lib/bigData";
+import multiSelect from '../../src/lib/MultiSelect';
+import filterColumn from '../../src/lib/filterColumn';
+import sort from '../../src/lib/sort';
+
+let  ComplexTable = filterColumn(BigData(multiSelect(Table, Checkbox)), Popover, Icon);
+
+const columns = [
+    {
+        title:'序号',
+        dataIndex:'index',
+        width:'50',
+        render:(text,record,index)=>{
+            return index
+        },
+        fixed:'left'
+    },
+    {
+    title: "用户名", dataIndex: "a", key: "a", width: 580, className: "rowClassName",
+    render: (text, record, index) => {
+      return (
+        <Tooltip inverse overlay={text}>
+          <span tootip={text} style={{
+            display: "inline-block",
+            width: "80px",
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+            whiteSpace: "nowrap",
+            verticalAlign: "middle",
+          }}>{text}</span>
+        </Tooltip>
+      );
+    }
+  },
+  { id: "123", title: "性别", dataIndex: "b", key: "b", width: 80},
+  { title: "年龄", dataIndex: "c", key: "c", width: 200 },
+  {
+    title: "操作",
+    dataIndex: "d",
+    key: "d",
+    fixed:'right',
+    render(text, record, index) {
+      return (
+        <div style={{ position: 'relative' }} title={text} >
+          <a
+            href="javascript:;"
+            tooltip={text}
+            onClick={() => {
+              alert('这是第' + index + '列，内容为:' + text);
+            }}
+          >
+            一些操作
+              </a>
+        </div>
+      );
+    }
+  }
+];
+
+const data = [ ...new Array(10000) ].map((e, i) => {
+    const rs = { a: i + 'a', b: i + 'b', c: i + 'c', d: i + 'd', key: i };
+    if(i%3==0){
+        rs.b = '女';
+    }
+    return rs;
+   })
+
+
+class Demo32 extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data,
+      selectedRowIndex: 0
+    }
+  }
+
+  render() {
+    return (
+        <ComplexTable
+          columns={columns}
+          data={data}
+          parentNodeId='parent'
+          scroll={{y:300}}
+          height={40}
+          onRowClick={(record, index, indent) => {
+            this.setState({
+              selectedRowIndex: index
+            });
+          }}
+        />
+
+     
+    );
+  }
+}
+
+export default Demo32;
