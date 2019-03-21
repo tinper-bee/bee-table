e43809218cda8f4f2d3b9d19604552386a7917bb
liushaozhen <804199994@qq.com>
2019-03-21 10:02:37 +0800

    demo修改

--- demo/index-demo-base.js
+++ demo/index-demo-base.js
@@ -1,14 +1,11 @@
-
-import { Con, Row, Col } from 'bee-layout';
-import { Panel } from 'bee-panel';
-import Button from 'bee-button';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Con, Row, Col } from 'bee-layout';
+import { Panel } from 'bee-panel';
+import Drawer from 'bee-drawer';
+import Clipboard from 'bee-clipboard'; 
+import Button from '../src';
 
-
-const CARET = <i className="uf uf-arrow-down"></i>;
-
-const CARETUP = <i className="uf uf-arrow-up"></i>;
 
 
 {demolist}
@@ -19,36 +16,48 @@ class Demo extends Component {
         this.state = {
             open: false
         }
-        this.handleClick = this.handleClick.bind(this);
     }
-    handleClick() {
+    handleClick=()=> {
         this.setState({ open: !this.state.open })
+    }
+    fCloseDrawer=()=>{
+        this.setState({
+            open: false
+        })
     }
 
     render () {
         const { title, example, code, desc, scss_code  } = this.props;
-        let caret = this.state.open ? CARETUP : CARET;
-        let text = this.state.open ? "隐藏代码" : "查看代码";
 
         const header = (
             <div>
-                {example}
-                <Button style={{"marginTop": "10px"}} shape="block" onClick={ this.handleClick }>
-                    { caret }
-                    { text }
-                </Button>
+                <p className='component-title'>{ title }</p>
+                <p>{ desc }</p>
+                <span className='component-code' onClick={this.handleClick}> 查看源码 <i className='uf uf-arrow-right'/> </span>
             </div>
         );
         return (
-            <Col md={12} id={title.trim()} >
-                <h3>{ title }</h3>
-                <p>{ desc }</p>
-                <Panel copyable collapsible headerContent expanded={ this.state.open } colors='bordered' header={ header } footerStyle = {{padding: 0}}>
-                    <pre><code className="hljs javascript">{ code }</code></pre>
-                    { !!scss_code ? <pre><code className="hljs css">{ scss_code }</code></pre> : null }
-                </Panel>
-            </Col>
-        )
+            <Col md={12} id={title.trim()} className='component-demo'>
+            <Panel header={header}>
+                {example}
+            </Panel>
+           
+            <Drawer className='component-drawerc' title={title} show={this.state.open} placement='right' onClose={this.fCloseDrawer}>
+            <div className='component-code-copy'> JS代码 
+                <Clipboard action="copy" text={code}/>
+            </div>
+            <pre className="pre-js">
+                <code className="hljs javascript">{ code }</code>
+            </pre >
+            {!!scss_code ?<div className='component-code-copy copy-css'> SCSS代码 
+                <Clipboard action="copy" text={scss_code}/>
+            </div>:null }
+                { !!scss_code ? <pre className="pre-css">
+                 <code className="hljs css">{ scss_code }</code>
+                 </pre> : null }
+            </Drawer>
+        </Col>
+    )
     }
 }
 
@@ -58,16 +67,16 @@ class DemoGroup extends Component {
     }
     render () {
         return (
-                <Row>
-                    {DemoArray.map((child,index) => {
+            <Row>
+            {DemoArray.map((child,index) => {
 
-                        return (
-                            <Demo example= {child.example} title= {child.title} code= {child.code} scss_code= {child.scss_code} desc= {child.desc} key= {index}/>
-                        )
+                return (
+            <Demo example= {child.example} title= {child.title} code= {child.code} scss_code= {child.scss_code} desc= {child.desc} key= {index}/>
+    )
 
-                    })}
-                </Row>
-        )
+    })}
+    </Row>
+    )
     }
 }
 
