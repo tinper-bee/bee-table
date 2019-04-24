9b81845b3d386b9dd7c18200efcce31dbe9b7256
yangchch6 <yangchch6@yonyou.com>
2019-04-24 14:15:59 +0800

    删除搜索示例

--- demo/demolist/Demo1101.js
+++ demo/demolist/Demo1101.js
@@ -27,9 +27,9 @@ const columns16 = [
       );
     }
   },
-  { title: "用户名", dataIndex: "a", key: "a", width: 250 },
-  { id: "123", title: "性别", dataIndex: "b", key: "b", width: 100 },
-  { title: "年龄", dataIndex: "c", key: "c", width: 200 },
+  { title: "订单编号", dataIndex: "a", key: "a", width: 250 },
+  { id: "123", title: "单据日期", dataIndex: "b", key: "b", width: 100 },
+  { title: "供应商", dataIndex: "c", key: "c", width: 200 },
   
 ];
 const columns17 = [
@@ -48,16 +48,16 @@ const columns17 = [
       );
     }
   },
-  { title: "用户名", dataIndex: "a", key: "a", width: 100 },
-  { id: "123", title: "性别", dataIndex: "b", key: "b", width: 100 },
-  { title: "年龄", dataIndex: "c", key: "c", width: 200 },
+  { title: "订单编号", dataIndex: "a", key: "a", width: 100 },
+  { id: "123", title: "单据日期", dataIndex: "b", key: "b", width: 100 },
+  { title: "供应商", dataIndex: "c", key: "c", width: 200 },
   
 ];
 
 const data16 = [
-  { a: "令狐冲", b: "男", c: 41, d: "操作", key: "1" },
-  { a: "杨过", b: "男", c: 67, d: "操作", key: "2" },
-  { a: "郭靖", b: "男", c: 25, d: "操作", key: "3" }
+  { a: "NU0391001", b: "2019-03-01", c: "xx供应商", d: "操作", key: "1" },
+  { a: "NU0391002", b: "2018-11-02", c: "yy供应商", d: "操作", key: "2" },
+  { a: "NU0391003", b: "2019-05-03", c: "zz供应商", d: "操作", key: "3" }
 ];
 
 
@@ -86,15 +86,15 @@ class Demo16 extends Component {
     if(expanded){
       if(record.key==='1'){
         new_obj[record.key] = [
-          { a: "令狐冲", b: "男", c: 41, d: "操作", key: "1" },
-          { a: "杨过", b: "男", c: 67, d: "操作", key: "2" }
+          { a: "NU0391056", b: "2019-03-01", c: "gys1", d: "操作", key: "1" },
+          { a: "NU0391057", b: "2018-11-02", c: "gys2", d: "操作", key: "2" },
         ]
         this.setState({
           data_obj:new_obj
         })
       }else{
         new_obj[record.key] = [
-          { a: "令狐冲", b: "男", c: 41, d: "操作", key: "1" }
+          { a: "NU0391079", b: "2019-04-17", c: "gys5", d: "操作", key: "3" },
         ]
         this.setState({
           data_obj:new_obj
